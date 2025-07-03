bffdb182610273f44720c6a24c788169ca8eaa86
24DavidM <151885025+24DavidM@users.noreply.github.com>
2025-07-03 16:32:40 -0500

    CAMBIOS V4

--- Frontend-vet/src/hooks/useFetch.js
+++ Frontend-vet/src/hooks/useFetch.js
@@ -1,28 +1,31 @@
-                        import axios from 'axios';
+import axios from "axios";
+import { toast } from "react-toastify";
 
-import {toast} from 'react-toastify';
-
-function useFetch(){
-    const fetchDataBackend = async (url, form = null, method = 'POST') => {
+function useFetch() {
+    const fetchDataBackend = async (url, data = null, method = "GET",headers = {}) => {
+        const loadingToast = toast.loading("Procesando solicitud...");
         try {
-            let respuesta
-            if (method === 'POST') {
-                respuesta = await axios.post(url,form)
+            const options = {
+            method,
+            url,
+            headers: {
+                "Content-Type": "application/json",
+                ...headers,
+            },
+            data,
             }
-            else if(method === 'GET'){
-                respuesta = await axios.get(url) 
-            }
-            toast.success(respuesta?.data?.msg)
-            return respuesta?.data
-            
+            const response = await axios(options)
+            toast.dismiss(loadingToast); 
+            toast.success(response?.data?.msg)
+            return response?.data
         } catch (error) {
+            toast.dismiss(loadingToast); 
+            console.error(error)
             toast.error(error.response?.data?.msg)
-            const errorMsg = error.response?.data?.msg || 'Error con el servidor'
-            throw new Error (errorMsg)
         }
     }
-    return {fetchDataBackend}
+
+    return { fetchDataBackend }
 }
 
-
-export default useFetch
+export default useFetch;
--- Frontend-vet/src/pages/Create.jsx
+++ Frontend-vet/src/pages/Create.jsx
@@ -1,4 +1,4 @@
-import { Form } from '../components/create/Form'
+import { Form } from "../components/create/Form"
 
 const Create = () => {
     return (
@@ -6,7 +6,7 @@ const Create = () => {
             <h1 className='font-black text-4xl text-gray-500'>Agregar</h1>
             <hr className='my-4 border-t-2 border-gray-300' />
             <p className='mb-8'>Este módulo te permite gestionar registros</p>
-            <Form />
+            <Form></Form>
         </div>
     )
 }
